233dadfac93fe308abfcb4bb468c93f91e6fa96c
Chris Donahue <christopherdonahue@gmail.com>
2018-04-04 02:22:04 -0700

    Added environment script

---
+++ env.sh
@@ -0,0 +1,5 @@
+export THEANO_FLAGS='device=cuda,floatX=float32'
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-8.0/lib64:/usr/local/lib
+export LD_FLAGS=$LD_FLAGS:/usr/local/cuda-8.0/lib64
+export CPATH=$CPATH:/usr/local/cuda-8.0/include
+export CUDA_PATH=/usr/local/cuda-8.0
